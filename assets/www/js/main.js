2ad3941c7c90b2797542100504f676ec3b2862a4
Ronaldo Barnes <krazydragon@fullsail.com>
2013-05-01 14:59:50 -0700

    added web support files

---
+++ assets/www/js/main.js
@@ -0,0 +1,23 @@
+// Project 4
+// Author: Ronaldo Barnes
+// Created for: MDF3
+
+
+    
+
+$(document).ready(function(){    
+
+
+});
+
+
+ // Show alert 
+    function showAlert() {
+               alert("I am an alert box!");       
+      
+    }     
+
+   
+
+
+
